318d4658f8acb8a83f4c6e9bdf3574cec678fdaa
PrestonCazier <32598096+PrestonCazier@users.noreply.github.com>
2018-02-06 10:40:27 -0800

    Update telephone.py

--- scripts/telephone.py
+++ scripts/telephone.py
@@ -1,1 +1,16 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String
 
+pub = rospy.Publisher('student2', String, queue_size=10)
+def callback(data):
+	my_str = data.data
+	pub.publish(my_str)
+        
+def listener():
+    rospy.init_node('telephone1', anonymous=True)
+    rospy.Subscriber("student1", String, callback)
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
